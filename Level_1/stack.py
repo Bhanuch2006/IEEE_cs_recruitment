811836547e836f28c8d463a7a30055aef951e4bc
Bhanuch2006 <maheshbhanu.chekuri@gmail.com>
2025-03-07 16:42:37 +0530

    Update stack.py
    Successfully completing the level 1 using python

--- Level_1/stack.py
+++ Level_1/stack.py
@@ -1,16 +1,59 @@
-class MinMaxStack:
+class mystack:
     def __init__(self):
-        self.stack = [] 
-        self.min_stack = [] 
-        self.max_stack = []  
-
-    def push(self, x):
-
+        self.stack=[]
+        self.stack_min=[]
+        self.stack_max=[]
+    def push(self,x):
         self.stack.append(x)
+        if not self.stack_min or x<=self.stack_min[-1]:
+            self.stack_min.append(x)
+        if not self.stack_max or x>=self.stack_max[-1]:
+            self.stack_max.append(x)
+    def pop(self):
+        if not self.stack:
+            print("Cannot pop from an empty Stack")
+        p=self.stack.pop()
+        if p==self.stack_min[-1]:
+            self.stack_min.pop()
+        if p==self.stack_max[-1]:
+            self.stack_max.pop()
+        return p
+    def top(self):
+        if not self.stack:
+            print("Stack is empty")
+            return 
+        t=self.stack[-1]
+        return t
+    def bottom(self):
+        if not self.stack:
+            print("Stack is empty")
+        return self.stack[0]
+    def getmin(self):
+        if not self.stack_min:
+            print("Min not found")
+        return self.stack_min[-1]
+    def getmax(self):
+        if not self.stack_max:
+            print("Max not found")
+        return self.stack_max[-1]
+    def printit(self):
+        for i in range(len(self.stack)):
+            if i==0:
+                print(self.stack[i],end="")
+            print(" -",self.stack[i],end="")
+stack = mystack()
+stack.push(3)
+stack.push(5)
+stack.push(6)
+stack.push(1)
+# stack.push(0)
+stack.printit()
+m=stack.getmin()
+print(m)
+t=stack.top()
+print(t)
+print(stack.bottom())
 
-        if not self.min_stack or x <= self.min_stack[-1]:
-            self.min_stack.append(x)
 
 
-        if not self.max_stack or x >= self.max_stack[-1]:
-            self.max_stack.append(x)
+        
